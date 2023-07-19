617435469382c1649f6967dd7d7a4ee647067808
Hugidonic <netvaca@ya.ru>
2023-07-19 21:36:46 +0300

    [feature_dictionary]: Converters to\from JSON created

---
+++ feature_dictionary/src/main/java/com/hugidonic/feature_dictionary/data/local/Converters.kt
@@ -0,0 +1,28 @@
+package com.hugidonic.feature_dictionary.data.local
+
+import androidx.room.ProvidedTypeConverter
+import androidx.room.TypeConverter
+import com.google.gson.reflect.TypeToken
+import com.hugidonic.feature_dictionary.data.util.JsonParser
+import com.hugidonic.feature_dictionary.domain.models.MeaningModel
+
+@ProvidedTypeConverter
+class Converters(
+    private val parser: JsonParser
+) {
+    @TypeConverter
+    fun fromMeaningsJson(json: String): List<MeaningModel> {
+        return parser.fromJson<ArrayList<MeaningModel>>(
+            json = json,
+            type = object : TypeToken<ArrayList<MeaningModel>>() {}.type
+        ) ?: emptyList()
+    }
+
+    @TypeConverter
+    fun toMeaningJson(meanings: List<MeaningModel>): String {
+        return parser.toJson(
+            obj = meanings,
+            type = object : TypeToken<ArrayList<MeaningModel>>() {}.type
+        ) ?: "[]"
+    }
+}
