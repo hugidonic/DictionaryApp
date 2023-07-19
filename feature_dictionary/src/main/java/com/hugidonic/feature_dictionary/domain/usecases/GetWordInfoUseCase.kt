636ecb265a8dcc680eb76dcd4ef34f6136e0b899
Hugidonic <netvaca@ya.ru>
2023-07-19 23:11:39 +0300

    [feature_dictionary_repository]: GetWordInfoUseCase created

---
+++ feature_dictionary/src/main/java/com/hugidonic/feature_dictionary/domain/usecases/GetWordInfoUseCase.kt
@@ -0,0 +1,16 @@
+package com.hugidonic.feature_dictionary.domain.usecases
+
+import com.hugidonic.core.util.Resource
+import com.hugidonic.feature_dictionary.domain.models.WordInfoModel
+import com.hugidonic.feature_dictionary.domain.repository.WordInfoRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class GetWordInfoUseCase(private val repository: WordInfoRepository) {
+    suspend operator fun invoke(word: String): Flow<Resource<List<WordInfoModel>>> {
+        if (word.isBlank()) {
+            return flow { }
+        }
+        return repository.getWordInfo(word = word)
+    }
+}
