29de8fb34e8f6f4132ecac0e7417dff89a4240ce
Hugidonic <netvaca@ya.ru>
2023-07-19 23:05:58 +0300

    [feature_dictionary_repository]: ApiService baseurl added

--- feature_dictionary/src/main/java/com/hugidonic/feature_dictionary/data/remote/ApiService.kt
+++ feature_dictionary/src/main/java/com/hugidonic/feature_dictionary/data/remote/ApiService.kt
@@ -2,9 +2,16 @@ package com.hugidonic.feature_dictionary.data.remote
 
 import com.hugidonic.feature_dictionary.data.remote.dto.WordInfoDto
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ApiService {
 
     @GET("/api/v2/entries/en/{word}")
-    fun getWordInfo(): List<WordInfoDto>
+    fun getWordInfo(
+        @Path("word") word: String
+    ): List<WordInfoDto>
+
+    companion object {
+        const val BASE_URL = "https://api.dictionaryapi.dev"
+    }
 }
