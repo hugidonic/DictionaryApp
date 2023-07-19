01084fc127e033cde9558df8502f692919e470f7
Hugidonic <netvaca@ya.ru>
2023-07-19 23:06:22 +0300

    [feature_dictionary_repository]: WordInfoRepository created

---
+++ feature_dictionary/src/main/java/com/hugidonic/feature_dictionary/data/repository/WordInfoRepositoryImpl.kt
@@ -0,0 +1,51 @@
+package com.hugidonic.feature_dictionary.data.repository
+
+import com.hugidonic.core.util.Resource
+import com.hugidonic.feature_dictionary.data.local.WordInfoDao
+import com.hugidonic.feature_dictionary.data.local.entities.WordInfoEntity
+import com.hugidonic.feature_dictionary.data.remote.ApiService
+import com.hugidonic.feature_dictionary.domain.models.WordInfoModel
+import com.hugidonic.feature_dictionary.domain.repository.WordInfoRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+
+class WordInfoRepositoryImpl(
+    private val api: ApiService,
+    private val dao: WordInfoDao
+) : WordInfoRepository {
+    override suspend fun getWordInfo(word: String): Flow<Resource<List<WordInfoModel>>> = flow {
+        emit(Resource.Loading())
+        val cachedWordInfos = dao.getWordsInfos(word).map { entity -> entity.toWordInfoModel() }
+        emit(Resource.Success(cachedWordInfos))
+
+        try {
+            val wordInfosFromApiDto = api.getWordInfo(word)
+            dao.deleteWordInfo(wordInfosFromApiDto.map { it.word })
+            dao.insertWordInfos(wordInfosFromApiDto.map { it.toWordInfoEntity() })
+
+        } catch (e: IOException) {
+            emit(
+                Resource.Error(
+                    message = "Oops, something went wrong!",
+                    data = cachedWordInfos
+                )
+            )
+        } catch (e: HttpException) {
+            emit(
+                Resource.Error(
+                    message = "Couldn't reach server. Check your internet connection",
+                    data = cachedWordInfos
+                )
+            )
+        }
+
+        val newWordInfosEntity = dao.getWordsInfos(word)
+
+        emit(Resource.Success(
+            data = newWordInfosEntity.map { it.toWordInfoModel() }
+        ))
+
+    }
+}
---
+++ feature_dictionary/src/main/java/com/hugidonic/feature_dictionary/domain/repository/WordInfoRepository.kt
@@ -0,0 +1,9 @@
+package com.hugidonic.feature_dictionary.domain.repository
+
+import com.hugidonic.core.util.Resource
+import com.hugidonic.feature_dictionary.domain.models.WordInfoModel
+import kotlinx.coroutines.flow.Flow
+
+interface WordInfoRepository {
+    suspend fun getWordInfo(word: String): Flow<Resource<List<WordInfoModel>>>
+}
