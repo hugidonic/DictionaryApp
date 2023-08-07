32c802165a4562a6231bbbd3cd77d31a4d5c000a
Hugidonic <netvaca@ya.ru>
2023-08-07 16:35:27 +0300

    [feature_dictionary]: MainActivity with AppNavGraph

--- app/src/main/java/com/hugidonic/dictionaryapp/MainActivity.kt
+++ app/src/main/java/com/hugidonic/dictionaryapp/MainActivity.kt
@@ -1,40 +1,60 @@
 package com.hugidonic.dictionaryapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.hugidonic.dictionaryapp.ui.theme.DictionaryAppTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.compose.rememberNavController
+import com.hugidonic.core.theme.DictionaryAppTheme
+import com.hugidonic.feature_dictionary.presentation.UIEvent
+import com.hugidonic.feature_dictionary.presentation.WordInfoViewModel
+import com.hugidonic.feature_dictionary.presentation.components.RecentSearch
+import com.hugidonic.feature_dictionary.presentation.components.SearchScreen
+import com.hugidonic.feature_dictionary.presentation.components.SearchTextField
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             DictionaryAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                val viewModel: WordInfoViewModel = hiltViewModel()
+                val navController = rememberNavController()
+                val scaffoldState = rememberScaffoldState()
+
+                LaunchedEffect(key1 = true) {
+                    viewModel.eventFlow.collectLatest { event ->
+                        when (event) {
+                            is UIEvent.ShowSnackbar -> {
+                                scaffoldState.snackbarHostState.showSnackbar(
+                                    message = event.message
+                                )
+                            }
+                        }
+                    }
+                }
+                Scaffold(
+                    scaffoldState = scaffoldState,
+                    topBar = {}
+                ) {
+                    AppNavGraph(navController = navController)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DictionaryAppTheme {
-        Greeting("Android")
     }
 }
